bd335e2298771b1ce440c5617826d887ad9afccc
lgavlich <lgavlich@gmail.com>
2021-08-16 14:11:58 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,9 @@
 import { onError } from './main';
-function fetchCountries(name) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
-        .then(response => {
-            return response.json();
-        },
-        ).catch(onerror);
+function fetchCountries(nameCountry) {
+  return fetch(`https://restcountries.eu/rest/v2/name/${nameCountry}`)
+    .then(response => {
+      return response.json();
+    })
+    .catch(onerror);
 }
 export default { fetchCountries };
